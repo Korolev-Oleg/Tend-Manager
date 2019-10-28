de3a2efc193d3fe15cfde9d4a5b0c3c8cfda82ba
Huston <1kor.oleg@gmail.com>
2019-10-29 00:59:46 +0300

    add general page

--- interface/variablesTab.py
+++ interface/variablesTab.py
@@ -19,6 +19,7 @@ class Variables(ExcelTab):
         self.lets = restoredData["variables"]['default']
         self.__updateTreeWidget()
         self.btn_save.clicked.connect(self._save)
+        self.generalInit()
 
     def displayDesired(self, data):
         if data[0] == '44':
@@ -66,4 +67,49 @@ class Variables(ExcelTab):
             tree.resizeColumnToContents(1)
 
     def generalInit(self):
-        pass
+        def update():
+            general = self.restoredData['general']
+            self.projectspath.setText(general['mainPath'])
+            self.paymentpath.setText(general['paymentPath'])
+            self.sheetName.setText(general['sheetName'])
+            self.cellTopLeft.setText(general['cellTopLeft'])
+            self.cellBotDn.setText(general['cellBotDn'])
+
+        def choseProjectpath():
+            text = r"Выберите основную папку проектов"
+            path = Qtw.QFileDialog.getExistingDirectory(self, text)
+            self.restoredData['general']['mainPath'] = path
+            update()
+
+        def chosePaymentpath():
+            text = r"Выберите файл расчета"
+            path = Qtw.QFileDialog.getOpenFileName(self, text, '', r"Документы (*.xlsx)")
+            print(path)
+            self.restoredData['general']['paymentPath'] = path[0]
+            update()
+
+        def datasave(obj):
+            sheetName = self.sheetName.text().strip()
+            self.restoredData['general']['sheetName'] = sheetName
+            if validateCells():
+                self.msg(0, "Неверный формат ячейки")
+            else:
+                cellTopLeft = self.cellTopLeft.text().strip()
+                self.restoredData['general']['cellTopLeft'] = cellTopLeft
+                cellBotDn = self.cellBotDn.text().strip()
+                self.restoredData['general']['cellBotDn'] = cellBotDn
+                self.msg(0, "Готово!", "")
+
+        def validateCells():
+            cell = self.cellTopLeft.text()
+            if not re.match(r'[A-Z{1,3}]{1,3}\d{1,3}', cell):
+                return True
+            cell = self.cellBotDn.text()
+            if not re.match(r'[A-Z{1,3}]{1,3}\d{1,3}', cell):
+                return True
+
+
+        self.btnProjectpath.clicked.connect(choseProjectpath)
+        self.btnPaymentpath.clicked.connect(chosePaymentpath)
+        self.saveDataGeneral.clicked.connect(datasave)
+        update()
