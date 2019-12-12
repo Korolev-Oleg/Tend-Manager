45ecb74010bfb8838e2e63a73be15a8e56e44626
hustinCun <1kor.oleg@gmail.com>
2019-12-12 10:33:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,15 @@ from interface              import mainUi
 from processing             import dbase, process
 
 if __name__ == '__main__':
-    restored = dbase.read()
-    localGeneral = restored['general']
-    localRestored = restored
 
-    shared = localGeneral['shared']
-    if shared:
-        print('shared')
+    # чтение локальной базы
+    localRestored = dbase.read()
+    localGeneral = localRestored['general']
+
+    if localGeneral['shared']:
+        
+        # расшареная база
         sharedRestored = dbase.read(localGeneral['shared'])
         form = mainUi.show(sharedRestored, localRestored)
     else:
-        form = mainUi.show(restored, localRestored)
-
-def form_init(form, restored, localGeneral):
-  
-    # process.start(form, restored, localGeneral)
-
-    shared = localGeneral['shared']
-    if shared:
-        try:
-            sharedRestored = restored
-            dbase.save(sharedRestored, shared)
-        except NameError:
-            print('Name error', shared)
-            pass
-
-    restored['general']['mainPath'] = localGeneral['mainPath']
-    dbase.save(restored)
+        form = mainUi.show(localRestored, localRestored)
