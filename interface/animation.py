ffd6fa95ad9b1692b1db1fdc968b16acf554c47f
hustinCun <1kor.oleg@gmail.com>
2019-12-12 10:30:23 +0300

    add 2 positional animation

--- interface/animation.py
+++ interface/animation.py
@@ -4,19 +4,43 @@ class Window():
     """ Smoth win mover.
         
         window -> self:
+        mode -> 0, 1, 2  (none, left, right)
         
     """
-    def __init__(self, window):
+    def __init__(self, window, mode):
         self.wnd = window
         self.corn_status = 0
         self.popup_status = -1
+        self.MODE = mode
+        self.setup_position()
+
+    def setup_position(self):
+        if self.MODE == 2:
+            x = self.wnd.screen.width() - self.wnd.width() / 2
+        elif self.MODE == 1:
+            x = -self.wnd.width() + self.wnd.width() / 2
+        else:
+            return
+
+        y = self.wnd.screen.height() / 2 - self.wnd.height() / 2
+        self.wnd.move(x, y)
         
     def popup_corner_show(self):
         """ Show corner. """
         if not self.popup_status:
-            # print('popup_corner_show')
-            pos_start = self.wnd.screen.width() - 10
-            pos_end = self.wnd.screen.width() - 26
+            # right
+            if self.MODE == 2:
+                pos_start = self.wnd.screen.width() - 10
+                pos_end = self.wnd.screen.width() - 26
+
+            # left
+            elif self.MODE == 1:
+                pos_start = (-self.wnd.width() + 10)
+                pos_end = (-self.wnd.width() + 26)
+
+            else:
+                return
+
             self.win_move(pos_start, pos_end)
             self.corn_status = 1
 
@@ -24,28 +48,67 @@ class Window():
         """ Hide corner. """
         if not self.popup_status:
             if self.corn_status:
-                pos_start = pos_end = self.wnd.screen.width() - 26
-                pos_end = self.wnd.screen.width() - 10
+                # right
+                if self.MODE == 2:
+                    pos_start = self.wnd.screen.width() - 26
+                    pos_end = self.wnd.screen.width() - 10
+
+                # left
+                elif self.MODE == 1:
+                    pos_start = -self.wnd.width() + 26
+                    pos_end = -self.wnd.width() + 10
+
+                else:
+                    return
+                    
                 self.win_move(pos_start, pos_end)
                 self.corn_status = 0
 
     def popup_show_full(self):
         """ Show full window. """
         if self.popup_status == 2:
+            # right
+            if self.MODE == 2:
+                width = self.wnd.screen.width() - self.wnd.width() / 2
+
+            # left
+            elif self.MODE == 1:
+                width = 0
+            
+            else:
+                return
+
             height = self.wnd.screen.height() / 2 - self.wnd.height() / 2
-            width = self.wnd.screen.width() - self.wnd.width() / 2
             self.wnd.move(width, height)
             self.popup_status = 1
         else:
+            # right
+            if self.MODE == 2:
+                width = self.wnd.screen.width() - self.wnd.width()
+            
+            # left
+            elif self.MODE == 1:
+                width = 0
+
             height = self.wnd.screen.height() / 2 - self.wnd.height() / 2
-            width = self.wnd.screen.width() - self.wnd.width()
             self.wnd.move(width, height)
             self.popup_status = 2
 
     def popup_show(self):
         """ Show 1/2 window. """
-        pos_start = pos_end = self.wnd.screen.width() - 26
-        pos_end = self.wnd.screen.width() - self.wnd.width() / 2
+        # right
+        if self.MODE == 2:
+            pos_start = self.wnd.screen.width() - 26
+            pos_end = self.wnd.screen.width() - self.wnd.width() / 2
+        
+        # left
+        elif self.MODE == 1:
+            pos_start = -self.wnd.width() + 26
+            pos_end = -self.wnd.width() + self.wnd.width() / 2
+        
+        else:
+            return
+
         self.win_move(pos_start, pos_end, 1000)
         self.popup_status = 1
 
@@ -53,17 +116,33 @@ class Window():
         """ Hide window. """
         # половина окна
         if self.popup_status == 1:
-            # print(' --1/2')
-            pos_start = self.wnd.screen.width() - self.wnd.width() / 2
-            pos_end = self.wnd.screen.width() - 10
+            # right
+            if self.MODE == 2:
+                pos_start = self.wnd.screen.width() - self.wnd.width() / 2
+                pos_end = self.wnd.screen.width() - 10
+
+            # left
+            elif self.MODE == 1:
+                pos_start = -self.wnd.width() + self.wnd.width() / 2
+                pos_end = -self.wnd.width() + 10
+            else:
+                return
         
         # все окно
         elif self.popup_status == 2:
-            # print(' --1')
-            pos_start = self.wnd.screen.width() - self.wnd.width()
-            pos_end = self.wnd.screen.width() - 10
+            # right
+            if self.MODE == 2:
+                pos_start = self.wnd.screen.width() - self.wnd.width()
+                pos_end = self.wnd.screen.width() - 10
+            
+            # left
+            elif self.MODE == 1:
+                pos_start = -self.wnd.width() + self.wnd.width()
+                pos_end = -self.wnd.width() + 10
 
-        # 
+            else:
+                return
+
         else:
             return
 
